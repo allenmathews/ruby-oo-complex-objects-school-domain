a86cd8198555ff6dba424fa093b1d4472ff4440a
Allen Mathews <allenjoemathews@gmail.com>
2021-09-07 13:03:10 +1000

    done with assignment

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+    def initialize (school)
+        @school = school
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        # if roster[grade] == nil
+        #     roster[grade] = [name]
+        # else
+        #     roster[grade] << name
+        # end
+        roster[grade] ||=[]
+        roster[grade] << name
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |grade, students|
+        roster[grade] = students.sort  
+    end
+    roster
+end
+
+end
+
+
